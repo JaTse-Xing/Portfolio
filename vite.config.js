560246623ec3bb97fdad094e0f10391d8cabc384
Ja tse xing <forlornsovereign@gmail.com>
2024-06-13 16:31:58 +0530

    Deploy Tailwind CSS with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  base: '/',
+  base: '/',  // Adjust if deploying to a subdirectory
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -12,8 +22,8 @@ export default defineConfig({
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
